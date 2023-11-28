7dd8ee14c98b7eee91e6918c09cfe061b783df6a
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-11-28 13:04:30 -0500

    Add the route for the API get request

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :api, defaults: { format: :json } do
+    get '/greetings/pick', to: 'greetings#pick'
+  end  
   # Defines the root path route ("/")
   # root "posts#index"
 end
